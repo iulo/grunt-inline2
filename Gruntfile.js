860bbb1911349299346016937cfc109bfb2426a4
iulo <liulong0130@gmail.com>
2016-07-20 00:21:10 +0800

    change version

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,11 +40,6 @@ module.exports = function(grunt) {
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  // grunt.loadNpmTasks('grunt-contrib-jshint');
-  // grunt.loadNpmTasks('grunt-contrib-clean');
-  // grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-    grunt.registerTask('test', ['inline2']);
-    grunt.registerTask('default', ['test']);
+  grunt.registerTask('test', ['inline2']);
+  grunt.registerTask('default', ['test']);
 };
